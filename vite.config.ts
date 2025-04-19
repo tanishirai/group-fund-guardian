bb530115da69b25710b1c128ffbdea5c8cfe6c2d
tanishirai <tanishirai2604@gmail.com>
2025-04-19 15:28:15 +0530

    vite file updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: [
-      "315dfdb8-f7d2-40cc-b3d2-113f3dadc2dd.lovableproject.com"
-    ],
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
-
+});
